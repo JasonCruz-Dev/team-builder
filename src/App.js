dc52a8de46b35644cc5005815f214a4432c7ff6a
Jason Cruz <badmonkey.dev@gmail.com>
2019-12-12 01:33:45 -0600

    added initial team form state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React from "react";
 import "./App.css";
 import Form from "./components/Form";
 
+const intitalTeamList = [
+  {
+    id: uuid(),
+    name: `Jason`,
+    email: `jcruz.bmt@gmail.com`,
+    role: `Software Engineer`
+  }
+];
+
+const initialTeamForm = [
+  {
+    name: "",
+    email: "",
+    role: ""
+  }
+];
+
 function App() {
   return (
     <div className='App'>
